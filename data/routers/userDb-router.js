d1e441a95c5f4de41832127c6b6f3f1919889634
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-20 18:55:30 -0400

    GET all users and GET single user seems to work.

--- data/routers/userDb-router.js
+++ data/routers/userDb-router.js
@@ -23,37 +23,37 @@ router.post('/', (req, res) => {
   }
 });
 
-// router.get('/', (req, res) => {
-//   postDb
-//     .get()
-//     .then(posts => {
-//       res.status(200).json(posts);
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: 'The posts information could not be retrieved.' });
-//     });
-// });
+router.get('/', (req, res) => {
+  userDb
+    .get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' });
+    });
+});
 
-// router.get('/:id', (req, res) => {
-//   postDb
-//     .getById(req.params.id)
-//     .then(post => {
-//       if (post) {
-//         res.status(200).json(post);
-//       } else {
-//         res
-//           .status(404)
-//           .json({ error: 'The post with the specified ID does not exist.' });
-//       }
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: 'The post information could not be retrieved.' });
-//     });
-// });
+router.get('/:id', (req, res) => {
+  userDb
+    .getById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ error: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
+    });
+});
 
 // router.delete('/:id', (req, res) => {
 //   postDb
