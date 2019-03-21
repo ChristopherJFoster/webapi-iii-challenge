41b71d889321ba2cb14ec97f5a941cae96c71b0b
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-21 18:18:14 -0400

    Added cors package.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
@@ -6,6 +7,7 @@ const postDbRouter = require('./data/routers/postDb-router');
 const userDbRouter = require('./data/routers/userDb-router');
 
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/posts', postDbRouter);
 server.use('/api/users', userDbRouter);
